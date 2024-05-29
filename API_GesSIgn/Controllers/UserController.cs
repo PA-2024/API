ef32f937f905fa690f466f5537256067342d3a3f
NicolasDebras <nicodebras17@gmail.com>
2024-05-29 20:56:39 +0200

    add GetProfByToken : method for recup all prof of your school

--- API_GesSIgn/Controllers/UserController.cs
+++ API_GesSIgn/Controllers/UserController.cs
@@ -109,6 +109,7 @@ namespace API_GesSIgn.Controllers
             }
         }
 
+        // GET: api/User/bytoken/
         [HttpGet("bytoken/")]
         public async Task<ActionResult<IEnumerable<User>>> GetMyUsersByToken() {
             
@@ -121,6 +122,37 @@ namespace API_GesSIgn.Controllers
                     .ToListAsync();
             return Ok(users);
         }
-        
+
+        // GET : api/User/Prof
+        [HttpGet("Prof/")]
+        public async Task<ActionResult<IEnumerable<User>>> GetProfByToken()
+        {
+            var roleName = User.FindFirst(ClaimTypes.Role)?.Value;
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            if (roleName == "Gestion Ecole")
+            {
+                var currentUser = await _context.Users
+                    .Include(u => u.User_School)
+                    .FirstOrDefaultAsync(u => u.User_Id == userId);
+                if (currentUser == null || currentUser.User_School == null)
+                {
+                    return BadRequest("User or user's school not found.");
+                }
+
+                // Return users with the role "Gestion Ecole"
+                var users = await _context.Users
+                    .Include(u => u.User_Role)
+                    .Where(u => u.User_Role.Role_Name == "Professeur")
+                    .Where(u => u.User_School_Id == currentUser.User_School_Id)
+                    .ToListAsync();
+                return Ok(users);
+            }
+            else
+            {
+                return new ObjectResult("Vous ne possédez pas les droits pour voir la liste.") { StatusCode = 403 };
+            }
+
+        }
     }
 }
