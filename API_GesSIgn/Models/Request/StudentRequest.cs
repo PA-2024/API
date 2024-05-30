06d71850668e98d0bb1e4a36d252ac99f1b7a1cf
NicolasDebras <nicodebras17@gmail.com>
2024-05-30 22:54:31 +0200

    add StudentRequest

---
+++ API_GesSIgn/Models/Request/StudentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace API_GesSIgn.Models.Request
+{
+    public class StudentRequest
+    {
+        public int Student_User_id { get; set; }
+
+        public int Student_Class_id { get; set; }
+    }
+}
