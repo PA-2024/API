0290a4ce96f08fb14a70ba7ad34d5b279758d590
debrasnicolas <nicodebras17@gmail.com>
2024-06-24 19:04:10 +0200

    je change de pc

--- API_GesSIgn/Models/Response/SubjectDetailsDto.cs
+++ API_GesSIgn/Models/Response/SubjectDetailsDto.cs
@@ -42,6 +42,24 @@ namespace API_GesSIgn.Models.Response
         }
     }
 
+
+    public class StudentIsPresent
+    {
+        public int Student_Id { get; set; }
+        public UserSimplifyDto Student_User { get; set; }
+        public bool IsPresent { get; set; }
+    }
+
+    public class SubjectsHourDetailsWithStudentsDto
+    {
+        public int SubjectsHour_Id { get; set; }
+        public DateTime SubjectsHour_DateStart { get; set; }
+        public DateTime SubjectsHour_DateEnd { get; set; }
+        public string SubjectsHour_Room { get; set; }
+        public SubjectDetailsWithOutStudentSimplifyDto Subject { get; set; }
+        public List<StudentSimplifyDto> Students { get; set; }
+    }
+
     /// <summary>
     /// Détails des heures de matières
     /// </summary>
