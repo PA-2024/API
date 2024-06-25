83567d274c62da96b717a0c1225fda40446cbddc
debrasnicolas <nicodebras17@gmail.com>
2024-06-25 14:28:21 +0200

    add GetSubjectsHourByTeacherAndDateRange

--- API_GesSIgn/Models/Response/SubjectsHourDetailsDto.cs
+++ API_GesSIgn/Models/Response/SubjectsHourDetailsDto.cs
@@ -32,4 +32,28 @@
             };
         }   
     }
+
+
+    public class SubjectsHourSimplify
+    {
+        public int SubjectsHour_Id { get; set; }
+        public DateTime SubjectsHour_DateStart { get; set; }
+        public DateTime SubjectsHour_DateEnd { get; set; }
+        public string SubjectsHour_Room { get; set; }
+        public string? SubjectsHour_TeacherComment { get; set; }
+        public BuildingDto Building { get; set; }
+
+        public static SubjectsHourDetailsDto FromSubjectsHour(SubjectsHour subjectsHour)
+        {
+            return new SubjectsHourDetailsDto
+            {
+                SubjectsHour_Id = subjectsHour.SubjectsHour_Id,
+                SubjectsHour_DateStart = subjectsHour.SubjectsHour_DateStart,
+                SubjectsHour_DateEnd = subjectsHour.SubjectsHour_DateEnd,
+                SubjectsHour_Room = subjectsHour.SubjectsHour_Room,
+                SubjectsHour_TeacherComment = subjectsHour.SubjectsHour_TeacherComment,
+                Building = BuildingDto.FromBuilding(subjectsHour.SubjectsHour_Bulding),
+            };
+        }   
+    }
 }
