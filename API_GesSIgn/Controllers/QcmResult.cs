fec8636dad9146d3e5d57cd637c09e6e1fd18dfd
NicolasDebras <nicodebras17@gmail.com>
2024-07-15 22:29:04 +0200

    add StudentResultsQcmForOneQcm

--- API_GesSIgn/Controllers/QcmResult.cs
+++ API_GesSIgn/Controllers/QcmResult.cs
@@ -1,6 +1,7 @@
 ﻿using API_GesSIgn.Models.Response;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API_GesSIgn.Controllers
 {
@@ -17,6 +18,7 @@ namespace API_GesSIgn.Controllers
             _context = context;
         }
 
+        [RoleRequirement("Professeur")]
         [HttpGet("AllResultsQcmForOneQcm/{id}")]
         public async Task<ActionResult<IEnumerable<QcmResultResponce>>> AllResultsQcmForOneQcm(int id)
         {
@@ -41,11 +43,40 @@ namespace API_GesSIgn.Controllers
 
             return Ok(qcm);
         }
+
+        [RoleRequirement("Eleve")]
+        [HttpGet("StudentResultsQcmForOneQcm/{id}")]
+        public async Task<ActionResult<IEnumerable<QcmResultResponce>>> StudentResultsQcmForOneQcm(int id)
+        {
+            var userid = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
+            var qcm = await _context.QcmResult
+                .Include(q => q.QcmResult_Student)
+                .ThenInclude(q => q.Student_User)
+                .Where(m => m.QcmResult_QCM_Id == id && m.QcmResult_Student.Student_User.User_Id == Convert.ToInt32(userid)).ToListAsync();
+
+            List<QcmResultResponce> result = new List<QcmResultResponce>();
+
+            foreach (var q in qcm)
+            {
+                var qcmResult = new QcmResultResponce
+                {
+                    QcmResult_Student = StudentSimplifyDto.FromStudent(q.QcmResult_Student),
+                    QcmResult_Score = q.QcmResult_Score,
+                    QcmResult_Id = q.QcmResult_Id
+                };
+
+                result.Add(qcmResult);
+            }
+
+            return Ok(qcm);
+        }
         /// <summary>
         /// 
         /// </summary>
         /// <param name="id">id du qcm</param>
         /// <returns></returns>
+        [RoleRequirement("Professeur")]
         [HttpGet("AllResultsQcmForOneQcmDetails/{id}")]
         public async Task<ActionResult<IEnumerable<QcmResultDetails>>> AllResultsQcmForOneQcmDetails(int id)
         {
